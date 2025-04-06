957f8c2cc938a3a90a9cec25100d938c73c9a483
Konrad <kondykox@gmail.com>
2025-04-06 11:57:04 +0200

    Vite build

--- src/types/State.ts
+++
@@ -1,6 +0,0 @@
-import { TodosList, TodoStatus } from "./Todo";
-
-export type AppState = {
-  todos: TodosList;
-  clickedStatus: TodoStatus;
-};
--- src/types/Todo.ts
+++
@@ -1,8 +0,0 @@
-export type Todo = {
-  id: string;
-  name: string;
-  status: TodoStatus;
-};
-
-export type TodoStatus = "todo" | "doing" | "done";
-export type TodosList = { todo: Todo[]; doing: Todo[]; done: Todo[] };
